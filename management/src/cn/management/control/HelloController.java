140b7fa03579dd544091a1dd14f80868b9374299
qiuxinwubo <zcb1041804765@163.com>
2017-08-03 14:33:25 +0800

    springmvc搭建 freemarker搭建

---
+++ management/src/cn/management/control/HelloController.java
@@ -0,0 +1,20 @@
+package cn.management.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public @ResponseBody String test() {
+        return "hello, world! This com from spring!";
+    }
+	@RequestMapping("/freemaker")
+	public String freemaker() {
+		return "/shiyan/shiyan";
+	}
+}
