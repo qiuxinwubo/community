aa3758b2c35d5e9b1b81e1870972bd7011ac8c96
qiuxinwubo <zcb1041804765@163.com>
2017-08-03 17:01:03 +0800

    hibernate配置并实验

--- management/src/cn/management/control/HelloController.java
+++ management/src/cn/management/control/HelloController.java
@@ -1,13 +1,20 @@
 package cn.management.control;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.management.entity.SysUser;
 
 
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+    private SessionFactory sessionFactory;
 	
 	@RequestMapping("/hello")
 	public @ResponseBody String test() {
@@ -15,6 +22,16 @@ public class HelloController {
     }
 	@RequestMapping("/freemarker")
 	public String freemaker() {
+		return "/shiyan/shiyan";
+	}
+	@RequestMapping("/entity")
+	public String entity() {
+		SysUser user=new SysUser();
+//		user.setId("1111");
+		user.setIfDel("1");
+		user.setUsername("zhang");
+		user.setPassword("343492");
+		sessionFactory.openSession().save(user);
 		return "/shiyan/shiyan";
 	}
 }
